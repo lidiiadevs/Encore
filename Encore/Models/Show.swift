696b935415783ed02de04ea11411ef3ce8168d0d
Lidiia Diachkovskaia <lyatlasova@gmail.com>
2026-06-26 16:29:31 -0400

    new

---
+++ Encore/Models/Show.swift
@@ -0,0 +1,8 @@
+//
+//  Show.swift
+//  Encore
+//
+//  Created by Lidiia Diachkovskaia on 6/26/26.
+//
+
+import Foundation
---
+++ Encore/ViewModels/AttendedViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AttendedViewModel.swift
+//  Encore
+//
+//  Created by Lidiia Diachkovskaia on 6/26/26.
+//
+
+import Foundation
