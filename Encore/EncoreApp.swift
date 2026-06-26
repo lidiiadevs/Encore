307338144452c24a4757b3d17901d8cc3fbb3125
Lidiia Diachkovskaia <lyatlasova@gmail.com>
2026-06-26 15:25:39 -0400

    Initial Commit

---
+++ Encore/EncoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  EncoreApp.swift
+//  Encore
+//
+//  Created by Lidiia Diachkovskaia on 6/26/26.
+//
+
+import SwiftUI
+
+@main
+struct EncoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
