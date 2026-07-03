64d01fdcc63650240120dbd1e08e466a87931b6b
Lidiia Diachkovskaia <lyatlasova@gmail.com>
2026-07-03 13:22:56 -0400

    add UpcomingVIew

---
+++ Encore/ViewModels/UpcomingViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UpcomingViewModel.swift
+//  Encore
+//
+//  Created by Lidiia Diachkovskaia on 7/2/26.
+//
+
+import Foundation
---
+++ Encore/Views/ShowRowView.swift
@@ -0,0 +1,18 @@
+//
+//  ShowRowView.swift
+//  Encore
+//
+//  Created by Lidiia Diachkovskaia on 7/1/26.
+//
+
+import SwiftUI
+
+struct ShowRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShowRowView()
+}
