d2be71ad00d628383c35c7eb648cd3442fa9d67a
Lidiia Diachkovskaia <lyatlasova@gmail.com>
2026-07-07 17:21:00 -0400

    add ShowDetailView

---
+++ Encore/Views/ShowDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ShowDetailView.swift
+//  Encore
+//
+//  Created by Lidiia Diachkovskaia on 7/7/26.
+//
+
+import SwiftUI
+
+struct ShowDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShowDetailView()
+}
