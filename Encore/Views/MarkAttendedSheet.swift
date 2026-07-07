c9fc549573ff8e4c07fa27c66d8c561f8e468809
Lidiia Diachkovskaia <lyatlasova@gmail.com>
2026-07-07 16:07:13 -0400

    add markAsAttended

---
+++ Encore/Views/MarkAttendedSheet.swift
@@ -0,0 +1,18 @@
+//
+//  MarkAttendedSheet.swift
+//  Encore
+//
+//  Created by Lidiia Diachkovskaia on 7/7/26.
+//
+
+import SwiftUI
+
+struct MarkAttendedSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MarkAttendedSheet()
+}
