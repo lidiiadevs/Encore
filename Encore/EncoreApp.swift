900a444eebfc0a6ed4cafd1f8ca8ca78c501ac22
Lidiia Diachkovskaia <lyatlasova@gmail.com>
2026-07-10 18:32:51 -0400

    gif

--- Encore/EncoreApp.swift
+++ Encore/EncoreApp.swift
@@ -13,9 +13,9 @@ struct EncoreApp: App {
     
     @AppStorage("hasCompletedOnboarding") private var hasCompletedOnboarding = false
     
-//    init () {
-//        UserDefaults.standard.removeObject(forKey: "hasCompletedonboarding")
-//    } use this is u wanna see the onboarding page again got testing
+    init () {
+        UserDefaults.standard.removeObject(forKey: "hasCompletedonboarding")
+    } //use this is u wanna see the onboarding page again got testing
     
     var body: some Scene {
         WindowGroup {
