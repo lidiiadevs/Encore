dba279ec6d310d2d5025997a329a359e7b709370
Lidiia Diachkovskaia <lyatlasova@gmail.com>
2026-07-09 20:14:38 -0400

    refactor contenntUnavailableView

---
+++ Encore/ViewModels/SettingsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsViewModel.swift
+//  Encore
+//
+//  Created by Lidiia Diachkovskaia on 7/9/26.
+//
+
+import Foundation
---
+++ Encore/Views/EmptyStateView.swift
@@ -0,0 +1,18 @@
+//
+//  EmptyStateView.swift
+//  Encore
+//
+//  Created by Lidiia Diachkovskaia on 7/9/26.
+//
+
+import SwiftUI
+
+struct EmptyStateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmptyStateView()
+}
